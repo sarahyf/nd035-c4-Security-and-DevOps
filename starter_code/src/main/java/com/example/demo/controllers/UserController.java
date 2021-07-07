b23bd2b6f7689220fbaa6a440b0c166e01d07d79
sarahyf <alfadlysarah@gmail.com>
2021-07-07 17:24:37 +0300

    logs

--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +26,8 @@ import com.sun.tools.sjavac.Log;
 @RequestMapping("/api/user")
 public class UserController {
 	
+	public static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -53,10 +57,12 @@ public class UserController {
 		user.setCart(cart);
 		if (createUserRequest.getPassword().length() < 7
 				|| !createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			log.error("issue in the password");
 			return ResponseEntity.badRequest().build();
 		}
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
+		log.info("the user is created");
 		return ResponseEntity.ok(user);
 	}
 	
