8fab7b705896a56804e796808dab1d58ce39a90e
sarahyf <alfadlysarah@gmail.com>
2021-07-23 19:25:59 +0300

    fixing the code to get 401 error code instead of infinit loop

--- starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
+++ starter_code/src/main/java/com/example/demo/security/WebSecurityConfiguration.java
@@ -41,7 +41,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.parentAuthenticationManager(authenticationManagerBean()).userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
